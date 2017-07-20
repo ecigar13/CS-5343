0fca45de9c8fe448f7f114021c2fe36b6444e4b0
Keith Nguyen <khoanguyen0791@users.noreply.github.com>
2017-07-20 00:34:06 -0500

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,41 @@
+﻿// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include <cmath>
+#include <limits>
+#include <fstream>
+using namespace std;
+
+
+
+
+int main() 
+{
+	string fileName = "ConsoleApplication1.cpp";
+	string line;
+	fstream inFile;
+	inFile.open(fileName.c_str());
+	if (inFile.fail())
+	{
+		cout << "file doesn't exist or wrong name\n";
+		exit(1);
+	}
+
+	while (getline(inFile, line))
+	{
+		cout << line << endl;
+	}
+	inFile.close();
+
+
+
+	cout << "Enter key to exit." << endl;
+	cin.get();
+
+	return 0;
+}
