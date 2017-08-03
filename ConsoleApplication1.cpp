522fa7a56d78f85fff7f8db854001e8ff6cca67f
Keith Nguyen <khoanguyen0791@users.noreply.github.com>
2017-08-03 00:03:33 -0500

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,35 @@
+﻿// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//Example of stack from Data Structures & Algorithm, Goodrich.
+
+#include "stdafx.h"
+#include <iostream>
+#include<algorithm>
+#include <cmath>
+#include <random>
+#include <string>
+using namespace std;
+void throw_exception() 
+{
+	throw 10;
+}
+void my_unexpected() {
+	std::cout << "well - this was unexpected" << std::endl;
+}
+int main() 
+{
+	std::set_unexpected(my_unexpected);
+	try {
+		throw_exception();
+	}
+	catch (int x) {
+		std::cout << "catch int: " << x << std::endl;
+	}
+	catch (...) {
+		std::cout << "catch ..." << std::endl;
+	}
+
+	cout << "Enter key to exit." << endl;
+	cin.get();
+
+	return 0;
+}
