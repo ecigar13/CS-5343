49b205b8760a0eaabbcee7f4dd0d912766933deb
Keith Nguyen <khoanguyen0791@users.noreply.github.com>
2017-09-07 00:19:46 -0500

    Create assignment-1.cpp

---
+++ assignment-1.cpp
@@ -0,0 +1,36 @@
+/*1. Write a program to sort a single linked list to ascending order.  The linked list will have positive numbers in each node. Use selection sort algorithm .
+2. You will not use any library functions. You must create your own linked list. Initially the list will be unsorted.
+3. The list must have at least 15 nodes.
+4. Submit the code.
+5. Submit screen shots of execution.  You must print the traversed list before sorting.
+    Then print the traversed list after it is sorted. 
+    */
+#include <iostream>
+#include<algorithm>
+#include <cmath>
+#include <random>
+#include <string>
+#include <fstream>
+
+int main()
+{
+ifstream fin("list.in");
+ofstream fout("list.out");
+
+  
+  //open input file
+  //open output file
+  
+  //define struct
+  //read in and create linked list 
+  
+  //sort<-- problem
+  //retain the head
+  
+  //print output of the list
+  
+  //close output file
+  
+  fout.close();
+}
+
