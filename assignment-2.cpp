10ea8a7a98ebf143695343366d83f5e42a1e11f9
Keith Nguyen <khoanguyen0791@users.noreply.github.com>
2017-09-28 00:20:29 -0500

    Create assignment-2.cpp

---
+++ assignment-2.cpp
@@ -0,0 +1,49 @@
+/*
+ID: ecigar
+LANG: C++
+TASK: friday
+*/
+//#include "stdafx.h"
+#include <iostream>
+#include<algorithm>
+#include <fstream>
+#include "BinaryTree.h"
+using namespace std;
+
+int main() {
+
+	//must create unique random numbers first
+
+	ofstream fout("friday.out");
+	ifstream fin("friday.in");
+
+	streambuf * coutbuf = cout.rdbuf();
+	cout.rdbuf(fout.rdbuf());
+
+	int first;
+	fin >> first;
+
+	TreeNode* r =  new TreeNode(first);
+	BinaryTree t = BinaryTree(r);
+	while (fin >> first) {
+		r = new TreeNode(first);
+		t.insertLeaf(t.returnRoot(), r);
+	}
+
+	cout << "Inorder traversal: "; t.inorder(t.returnRoot()); cout << endl;
+	cout << "Preorder traversal: "; t.preorder(t.returnRoot()); cout << endl;
+	cout << "Postorder traversal: "; t.postorder(t.returnRoot()); cout << endl;
+
+	cout << "Delete.\n";
+	//delete fnction here.
+
+	cout << "Inorder traversal: "; t.inorder(t.returnRoot()); cout << endl;
+	cout << "Preorder traversal: "; t.preorder(t.returnRoot()); cout << endl;
+	cout << "Postorder traversal: "; t.postorder(t.returnRoot()); cout << endl;
+	t.print2DUtil(t.returnRoot(), 0);
+
+	fout.close();
+	cout.rdbuf(coutbuf);
+	//cin.get();
+	return 0;
+}
