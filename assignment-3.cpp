e5cf3733c3f500d02ab767e66a75ec9d8ff1bb2d
Keith Nguyen <khoanguyen0791@users.noreply.github.com>
2017-10-07 21:53:07 -0500

    Create assignment-3.cpp

---
+++ assignment-3.cpp
@@ -0,0 +1,38 @@
+#include "stdafx.h"
+#include <iostream>
+#include<algorithm>
+#include <fstream>
+#include <stdio.h>
+#include <string>
+
+//#include "BinaryTree.h"
+#include "Slist.h"
+#include "AvlTree.h"
+using namespace std;
+
+
+int main() {
+
+	ifstream fin("avl.in");
+	ofstream fout("avl.out");
+
+	streambuf * coutbuf = cout.rdbuf();
+	cout.rdbuf(fout.rdbuf());
+	int temp;
+	fin >> temp;
+	TreeNode* n = new TreeNode(temp);
+	AvlTree* t = new AvlTree(n);
+
+	while (fin >> temp) {
+		n = new TreeNode(temp);
+		cout << endl << "Insert: " << temp << endl;
+		t->insertLeaf(t->returnRoot(), n);
+	}
+	cout << "Preorder traversal: ";
+	t->preorder(t->returnRoot());
+	fout.close();
+	cout.rdbuf(coutbuf);
+	//cin.get();
+	return 0;
+}
+
