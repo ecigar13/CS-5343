742c6b1724fc3ed5830ae6e79a13a7ef1ffe28f8
Keith Nguyen <khoanguyen0791@users.noreply.github.com>
2017-10-07 21:52:46 -0500

    Update AvlTree.h

--- AvlTree.h
+++ AvlTree.h
@@ -13,6 +13,7 @@ class AvlTree :public BinaryTree {
 
 
 public:
+	AvlTree(TreeNode*n) :BinaryTree(n) {}
 	int height(TreeNode* n);
 	int diff(TreeNode* n);
 	int depth(TreeNode*n);
@@ -20,7 +21,127 @@ public:
 	TreeNode* rl_rotation(TreeNode* n);
 	TreeNode* ll_rotation(TreeNode* n);
 	TreeNode* lr_rotation(TreeNode* n);
-	TreeNode*balance(TreeNode* n);
-	TreeNode*insertLeaf(TreeNode* n) override;
-
+	TreeNode*rebalance(TreeNode* n);
+	TreeNode*insertLeaf(TreeNode* r, TreeNode* n);
+	void setHeight(TreeNode *n);
+	TreeNode* find_imbalance(TreeNode*n);
 };
+
+TreeNode * AvlTree::insertLeaf(TreeNode* r, TreeNode * n)
+{
+	TreeNode* temp = BinaryTree::insertLeaf(this->root, n);
+	temp = find_imbalance(n);
+	this->printTree(this->returnRoot(), 2);
+
+	if (temp != NULL) {
+		cout << "Imbalance node: " << temp->data << endl;
+		TreeNode* p = temp->parent;
+		if (temp == root) root = rebalance(root);
+		else if (p->left == temp) {
+			temp = rebalance(temp);
+			p->left = temp;
+		}
+		else {
+			temp = rebalance(temp);
+			p->right = temp;
+		}
+		cout << "After balance: " << endl;
+		this->printTree(this->returnRoot(), 2);
+
+	}
+	else {
+		cout << "No balancing" << endl;
+	}
+	return temp;
+}
+inline TreeNode * AvlTree::find_imbalance(TreeNode * n)
+{
+	int i = diff(n);
+	if (i < -1 || i>1) return n;
+
+	if (n == root) return NULL;
+
+	return find_imbalance(n->parent);
+}
+int AvlTree::height(TreeNode * n)
+{
+	int l_height = 0;
+	int r_height = 0;
+
+	if (n == NULL) return 0;
+	else {
+		r_height = 1 + height(n->right);
+		l_height = 1 + height(n->left);
+		return max(l_height, r_height);
+	}
+}
+int AvlTree::diff(TreeNode * n)
+{
+	return height(n->left) - height(n->right);
+}
+void AvlTree::setHeight(TreeNode * n)
+{
+	int l_height = height(n->left);
+	int r_height = height(n->right);
+	n->height = max(l_height, r_height);
+}
+int AvlTree::depth(TreeNode * n)
+{
+	if (n->parent != NULL) {
+		return 1 + depth(n->parent);
+	}
+	return 0;
+}
+TreeNode * AvlTree::rr_rotation(TreeNode * n)
+{
+	TreeNode*temp = n->right;
+	n->right = temp->left;
+	temp->parent = n->parent;
+	n->parent = temp;
+	temp->left = n;
+	return temp;
+}
+TreeNode * AvlTree::rl_rotation(TreeNode * n)
+{
+	n->right = ll_rotation(n->right);
+	return rr_rotation(n);
+}
+TreeNode * AvlTree::ll_rotation(TreeNode * n)
+{
+	TreeNode*temp = n->left;
+	n->left = temp->right;
+	temp->parent = n->parent;
+	n->parent = temp;
+	temp->right = n;
+	return temp;
+}
+TreeNode * AvlTree::lr_rotation(TreeNode * n)
+{
+	n->left = rr_rotation(n->left);
+	return ll_rotation(n);
+}
+TreeNode * AvlTree::rebalance(TreeNode * n)
+{
+	int bal = diff(n);
+	if (bal > 1) {
+		if (height(n->left->left) >= height(n->left->right)) {
+			n = ll_rotation(n);
+		}
+		else {
+			n = lr_rotation(n);
+		}
+	}
+	else if (bal < -1) {
+		if (height(n->right->right) >= height(n->right->left)) {
+			n = rr_rotation(n);
+		}
+		else {
+			n = rl_rotation(n);
+		}
+	}
+	else {
+		setHeight(n);
+	}
+	return n;
+}
+
