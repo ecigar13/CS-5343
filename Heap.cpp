2dae6eb94b0dd680d43ea0fbe797c154020e61e1
Keith Nguyen <khoanguyen0791@users.noreply.github.com>
2017-10-10 14:12:48 -0500

    Update Heap.cpp

--- Heap.cpp
+++ Heap.cpp
@@ -3,11 +3,24 @@
 class Heap{
 public:
 int child;
-int* children;
+vector<int>* heap;
 
 Heap(int keys);
-
+int parent(int i);
+  int left(int i);
+  int right(int i);
 };
 
-Heap::Heap(int keys){ child=keys;}
+Heap::Heap(int keys){ child=keys;
+                    heap= new vector(keys);}
+int Heap::left(int i){
+  return 2*i;
+}
 
+int Heap::right(int i){
+  return 2*i+1;
+}
+
+int Heap::parent(int i){
+  return floor(i/2);
+}
