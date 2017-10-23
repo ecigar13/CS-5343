9cf226cc110df7dd797a4d97c564f859f6392f5b
Keith Nguyen <khoanguyen0791@users.noreply.github.com>
2017-10-23 15:17:27 -0500

    Add files via upload

---
+++ Graph.h
@@ -0,0 +1,16 @@
+#pragma once
+
+
+#include <queue>
+
+
+//1. Write a program to do BFS traversal of a graph.
+//
+//You will first create a graph.Use Adjacency Matrix for graph representation.You may use a queue library to do the traversal.The graph must have at least 10 nodes and 15 edges.
+//
+//Submit screen shot of execution of the program.Also draw the graph on a paper and scan it and submitt it to show the graph you are doing BFS on.
+//2. Write a program that returns true if the graph is strongly connected.Else it returns true.
+//
+//Create the graph as in 1. above, using adjacency matrix.
+//
+//run the program on two graphs.A strongly connected graph and also a graph not strongly connected.Submit screen shot of execution for both the graphs.Also submit scanned copies of the graph.
