432ac6ffe557643d3e4cce16d633f80d2643bfa1
Keith Nguyen <khoanguyen0791@users.noreply.github.com>
2017-10-26 12:38:26 -0500

    Update assignment-3.cpp

--- assignment-3.cpp
+++ assignment-3.cpp
@@ -1,42 +1,46 @@
-//#include "stdafx.h"
-#include <iostream>
-#include<algorithm>
-#include <fstream>
-#include <stdio.h>
-#include <string>
+#include "stdafx.h"
 
-//#include "BinaryTree.h"
-//#include "Slist.h"
-#include "AvlTree.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <deque>
+#include "Graph.h"
 using namespace std;
 
-
+void createVector(vector<vector<int>> &temp, ifstream &fin) {
+	for (auto row : temp)
+		for (auto col : row)
+		{
+			fin >> col;
+			cout << col;
+		}
+}
 int main() {
+	//Test case: https://www.math.ucdavis.edu/~daddel/linear_algebra_appl/Applications/GraphTheory/GraphTheory_9_17/node9.html
 
 	ifstream fin("avl.in");
 	ofstream fout("avl.out");
 
 	streambuf * coutbuf = cout.rdbuf();
 	cout.rdbuf(fout.rdbuf());
-	int temp;
-	fin >> temp;
-	TreeNode* n = new TreeNode(temp);
-	AvlTree* t = new AvlTree(n);
-	t->preorder(t->returnRoot());
 
-	while (fin >> temp) {
-		n = new TreeNode(temp);
+	int vertices = 0;
+	fin >> vertices;
+	
+	vector<vector<int>> temp(vertices, vector<int>(vertices, 0));
 
-		cout << endl << "Insert: " << temp << endl;
-		t->insertLeaf(t->returnRoot(), n);
+	createVector(temp, fin);
 
-		cout << endl;
-	}
-	cout << "Preorder traversal: ";
-	t->preorder(t->returnRoot());
+	Mgraph* graph = new Mgraph(temp);
+	graph->printMatrix();
+
 	fout.close();
 	cout.rdbuf(coutbuf);
-	//cin.get();
-	return 0;
-}
 
+	//cin.get();
+
+
+
+
+
+}
